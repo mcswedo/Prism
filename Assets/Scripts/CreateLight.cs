63e60c8cebe588f78a2fcbbf72b9d3158665184c
Yangshin Ou <theguyoverthere6@gmail.com>
2017-01-21 11:04:17 -0800

    Added Grid pieces that turn.

---
+++ Assets/Scripts/CreateLight.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DigitalRuby.AnimatedLineRenderer
+{
+    public class CreateLight : MonoBehaviour {
+
+        public AnimatedLineRenderer AnimatedLine;
+        public GameObject wayPoint1;
+        public GameObject wayPoint2;
+        public GameObject wayPoint3;
+        float timer = 0;
+
+	    // Use this for initialization
+	    void Start () {
+		
+	    }
+	
+	    // Update is called once per frame
+	    void Update () {
+            timer += Time.deltaTime;
+
+            if(AnimatedLine == null)
+            {
+                return;
+            }
+            else if (timer > 3)
+            {
+                
+                AnimatedLine.Enqueue(wayPoint1.transform.position);
+                
+            }
+            else if(timer > 8)
+            {
+                Vector3 pos = wayPoint2.transform.position;
+                pos = Camera.main.ScreenToViewportPoint(new Vector3(pos.x, pos.y, AnimatedLine.transform.position.z));
+                AnimatedLine.Enqueue(pos);
+            }
+            else if (timer > 13)
+            {
+                Vector3 pos = wayPoint3.transform.position;
+                pos = Camera.main.ScreenToViewportPoint(new Vector3(pos.x, pos.y, AnimatedLine.transform.position.z));
+                AnimatedLine.Enqueue(pos);
+            }
+
+
+		
+	    }
+    }
+}
---
+++ Assets/Scripts/DetectConnection.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectConnection : MonoBehaviour {
+
+    public GameObject testObject;
+    bool connected = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        while(connected)
+        {
+            testObject.transform.Rotate(new Vector3(0, 0, 45));
+        }
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("Boop");
+        connected = true;
+        
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        connected = false;
+    }
+}
