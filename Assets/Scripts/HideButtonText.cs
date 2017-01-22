fd927624ccf442947982d4e8f4b8bd0c78290220
Michael <mcswedo@gmail.com>
2017-01-22 13:36:15 -0800

    Added level 2 to build settings, '3' button is locked

---
+++ Assets/Scripts/HideButtonText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HideButtonText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if(!gameObject.GetComponentInParent<Button>().interactable)
+		{
+			this.gameObject.SetActive(false);
+		}
+	}
+}
