bdcf9a12d966ea2674b09b557ba05789ce4c2116
Yangshin Ou <theguyoverthere6@gmail.com>
2017-01-22 10:36:50 -0800

    Revert "Major changes to prefabs, lights now are done by Raycast instead of node based collisions"
    This reverts commit 88a32b15580c5af2bd48a09a71e5a85474c84ed2.

--- Assets/Scripts/DetectConnection.cs
+++ Assets/Scripts/DetectConnection.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class DetectConnection : MonoBehaviour {
 
-    
+    public GameObject testObject;
+    bool connected = false;
     public Color sourceColor;
     public enum Sides {None,Left, Right, Above, Below };
 
@@ -15,107 +16,59 @@ public class DetectConnection : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+        if(connected)
+        {
+            
+        }
+        else
+        {
+           
+        }
 	}
 
     //Checks to see if is touching another connector.
     void OnTriggerStay2D(Collider2D collider)
     {
-        if(gameObject.tag == "Blank")
+        if (collider.gameObject.tag == "Connector")
         {
-            return;
-        }
-        if (collider.gameObject.tag == "Connector" )
-        {
-
-            switch (gameObject.GetComponentInParent<LightManager>().Block)
+            //if so, take the color of the other connecter if it's lit. 
+            connected = true;
+            if(collider.gameObject.GetComponentInParent<LightManager>().isComingFromSource && !gameObject.GetComponentInParent<LightManager>().isSource)
             {
-                case LightManager.BlockType.Source:
-                    gameObject.GetComponentInParent<LightManager>().adjacentNodes[0] = collider.transform.parent.gameObject;
-                    break;
-                case LightManager.BlockType.L:
-                    switch (side)
-                    {
-                        case Sides.Left:
-                            gameObject.GetComponentInParent<LightManager>().adjacentNodes[0] = collider.transform.parent.gameObject;
-                            break;
-                        case Sides.Above:
-                            gameObject.GetComponentInParent<LightManager>().adjacentNodes[1] = collider.transform.parent.gameObject;
-                            break;
-                        case Sides.Right:
-                            gameObject.GetComponentInParent<LightManager>().adjacentNodes[2] = null;
-                            break;
-                        case Sides.Below:
-                            gameObject.GetComponentInParent<LightManager>().adjacentNodes[3] = null;
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case LightManager.BlockType.Line:
-                    switch (side)
-                    {
-                        case Sides.Left:
-                            gameObject.GetComponentInParent<LightManager>().adjacentNodes[0] = collider.transform.parent.gameObject;
-                            break;
-                        case Sides.Above:
-                            gameObject.GetComponentInParent<LightManager>().adjacentNodes[1] = null;
-                            break;
-                        case Sides.Right:
-                            gameObject.GetComponentInParent<LightManager>().adjacentNodes[2] = collider.transform.parent.gameObject;
-                            break;
-                        case Sides.Below:
-                            gameObject.GetComponentInParent<LightManager>().adjacentNodes[3] = null;
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case LightManager.BlockType.T:
-                    switch (side)
-                    {
-                        case Sides.Left:
-                            gameObject.GetComponentInParent<LightManager>().adjacentNodes[0] = collider.transform.parent.gameObject;
-                            break;
-                        case Sides.Above:
-                            gameObject.GetComponentInParent<LightManager>().adjacentNodes[1] = collider.transform.parent.gameObject;
-                            break;
-                        case Sides.Right:
-                            gameObject.GetComponentInParent<LightManager>().adjacentNodes[2] = collider.transform.parent.gameObject;
-                            break;
-                        case Sides.Below:
-                            gameObject.GetComponentInParent<LightManager>().adjacentNodes[3] = null;
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case LightManager.BlockType.Cross:
-                    switch (gameObject.GetComponentInParent<LightManager>().sourceSide)
-                    {
-                        case LightManager.Sides.Left:
-                            gameObject.GetComponentInParent<LightManager>().adjacentNodes[0] = collider.transform.parent.gameObject;
-                            break;
-                        case LightManager.Sides.Above:
-                            gameObject.GetComponentInParent<LightManager>().adjacentNodes[1] = collider.transform.parent.gameObject;
-                            break;
-                        case LightManager.Sides.Right:
-                            gameObject.GetComponentInParent<LightManager>().adjacentNodes[2] = collider.transform.parent.gameObject;
-                            break;
-                        case LightManager.Sides.Below:
-                            gameObject.GetComponentInParent<LightManager>().adjacentNodes[3] = collider.transform.parent.gameObject;
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
+                gameObject.GetComponentInParent<LightManager>().isComingFromSource = true;
+                gameObject.GetComponentInParent<LightManager>().areLightsEnabled = true;
+                gameObject.GetComponentInParent<LightManager>().sourceSide = (LightManager.Sides)side;
+                sourceColor = collider.GetComponent<DetectConnection>().sourceColor;
+                gameObject.GetComponentInParent<LightManager>().colorSource = sourceColor;
             }
-        }   
-    }    
+            if(collider.gameObject.GetComponentInParent<LightManager>().isSource)
+            {
+                gameObject.GetComponentInParent<LightManager>().isComingFromSource = true;
+                gameObject.GetComponentInParent<LightManager>().areLightsEnabled = true;
+                gameObject.GetComponentInParent<LightManager>().sourceSide = (LightManager.Sides)side;
+                sourceColor = collider.GetComponentInParent<LightManager>().colorSource;
+                gameObject.GetComponentInParent<LightManager>().colorSource = sourceColor;
+            }
+
+        }
+        
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Connector")
+        {
+            connected = false;
+            
+                gameObject.GetComponentInParent<LightManager>().areLightsEnabled = false;
+                gameObject.GetComponentInParent<LightManager>().isComingFromSource = false;
+            
+            
+        }
+    }
 }
