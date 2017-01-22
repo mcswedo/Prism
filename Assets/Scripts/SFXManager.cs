daaa2a31c7f6e91610bb2a2b58ab0387a75523d7
Michael <mcswedo@gmail.com>
2017-01-21 23:02:52 -0800

    Minor fixes
    Exit button in Menu not having hover fx, click fx not playing properly,
    splash clicking upon load of menu

--- Assets/Scripts/SFXManager.cs
+++ Assets/Scripts/SFXManager.cs
@@ -5,17 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class SFXManager : MonoBehaviour 
 {
-
 	public AudioClip SFX_hover;
-	public AudioClip SFX_click;
 	public AudioSource sfxSource;
 
 	public void PlaySFX(string sfx)
 	{
 		if (sfx == "hover")
 			sfxSource.PlayOneShot(SFX_hover);
-
-		if (sfx == "click")
-			sfxSource.PlayOneShot(SFX_click);
 	}
 }
